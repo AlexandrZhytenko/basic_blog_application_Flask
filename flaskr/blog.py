b84d9d0be1ea4326381a2cf7a95468bb8716ccaf
Olexandr Zhytenko <zhytenko@ukr.net>
2018-05-31 17:45:52 +0300

    changes

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -10,9 +10,9 @@ bp = Blueprint("blog", __name__)
 def index():
     db = get_db()
     posts = db.execute(
-        "SELECT p.id, title, body, created, author_id, username\"
-        "FROM post p JOIN user u ON p.author_id = u.id\"
-        "ORDER BY created DESC"
+        "SELECT p.id, title, body, created, author_id, username"
+        " FROM post p JOIN user u ON p.author_id = u.id"
+        " ORDER BY created DESC"
     ).fetchall()
     return render_template("blog/index.html", posts=posts)
 
@@ -30,7 +30,7 @@ def create():
         else:
             db = get_db()
             db.execute(
-                "INSERT INTO post (title, body, author_id)\"
+                "INSERT INTO post (title, body, author_id)"
                 " VALUES (?, ?, ?)", (title, body, g.user["id"])
             )
             db.commit()
@@ -39,8 +39,8 @@ def create():
 
 def get_post(id, check_author=True):
     post = get_db().execute(
-        "SELECT p.id, title, body, created, author_id, username\"
-        " FROMost p JOIN user u ON p.author_ud = u.id\"
+        "SELECT p.id, title, body, created, author_id, username"
+        " FROM post p JOIN user u ON p.author_id = u.id"
         " WHERE p.id = ?", (id,)
     ).fetchone()
     if post is None:
@@ -64,12 +64,12 @@ def update(id):
         else:
             db = get_db()
             db.execute(
-                "UPDATE post SET title = ?, body = ?\"
+                "UPDATE post SET title = ?, body = ?"
                 " WHERE id = ?", (title, body, id)
             )
             db.commit()
             return redirect(url_for("blog.index"))
-    return render_template("blog/update,html", post=post)
+    return render_template("blog/update.html", post=post)
 
 @bp.route("/<int:id>/delete", methods=("POST",))
 @login_required
